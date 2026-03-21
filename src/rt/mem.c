537adc31a286103767585448b051f3b46393cef1
Hyoo Lim <hyoolim@gmail.com>
2026-03-21 10:40:52 -0400

    keep recycled per thread

--- src/rt/mem.c
+++ src/rt/mem.c
@@ -1,27 +1,37 @@
 #include "mem.h"
+#include <stdatomic.h>
 
 // new
-static int allocsize = 0;
-static size_t alloctotal = 0;
-static size_t allocused = 0;
-static size_t allocpeak = 0;
-static size_t allocrecycled = 0;
-static void *recycled[MEMORY_MAXRECYCLED];
+static _Atomic int allocsize = 0;
+static _Atomic size_t alloctotal = 0;
+static _Atomic size_t allocused = 0;
+static _Atomic size_t allocpeak = 0;
+static _Atomic size_t allocrecycled = 0;
+static _Thread_local void *recycled[MEMORY_MAXRECYCLED];
+
+static void update_peak(void) {
+    size_t used = atomic_load_explicit(&allocused, memory_order_relaxed);
+    size_t peak = atomic_load_explicit(&allocpeak, memory_order_relaxed);
+
+    while (used > peak) {
+        if (atomic_compare_exchange_weak_explicit(&allocpeak, &peak, used,
+                memory_order_relaxed, memory_order_relaxed))
+            break;
+    }
+}
 
 void *mem_alloc(size_t size) {
     assert(size > 0);
-    allocsize++;
+    atomic_fetch_add_explicit(&allocsize, 1, memory_order_relaxed);
 
     if (size < MEMORY_MAXRECYCLED && recycled[size] != NULL) {
         void *next = *(void **)recycled[size];
         void *new = recycled[size];
         recycled[size] = next;
         memset(new, 0x0, size);
-        allocused += size;
-        allocrecycled -= size;
-
-        if (allocused > allocpeak)
-            allocpeak = allocused;
+        atomic_fetch_add_explicit(&allocused, size, memory_order_relaxed);
+        atomic_fetch_sub_explicit(&allocrecycled, size, memory_order_relaxed);
+        update_peak();
         return new;
 
     } else {
@@ -30,11 +40,9 @@ void *mem_alloc(size_t size) {
         if (new == NULL)
             ERR("Unable to allocate mem!");
         *new = size;
-        alloctotal += size;
-        allocused += size;
-
-        if (allocused > allocpeak)
-            allocpeak = allocused;
+        atomic_fetch_add_explicit(&alloctotal, size, memory_order_relaxed);
+        atomic_fetch_add_explicit(&allocused, size, memory_order_relaxed);
+        update_peak();
         return new + 1;
     }
 }
@@ -42,34 +50,31 @@ void *mem_alloc(size_t size) {
 void mem_free(void *ptr) {
     if (ptr != NULL) {
         size_t size = *((size_t *)ptr - 1);
-        allocsize--;
+        atomic_fetch_sub_explicit(&allocsize, 1, memory_order_relaxed);
 
         if (size < MEMORY_MAXRECYCLED) {
             *(void **)ptr = recycled[size];
             recycled[size] = ptr;
-            allocused -= size;
-            allocrecycled += size;
+            atomic_fetch_sub_explicit(&allocused, size, memory_order_relaxed);
+            atomic_fetch_add_explicit(&allocrecycled, size, memory_order_relaxed);
 
         } else {
             size_t size2 = *(size_t *)(ptr = (size_t *)ptr - 1);
             free(ptr);
-            allocused -= size2;
+            atomic_fetch_sub_explicit(&allocused, size2, memory_order_relaxed);
         }
     }
 }
 
 void mem_freerecycled(void) {
-    int i;
-    void *curr;
-
-    for (i = 0; i < MEMORY_MAXRECYCLED; i++) {
-        curr = recycled[i];
+    for (int i = 0; i < MEMORY_MAXRECYCLED; i++) {
+        void *curr = recycled[i];
 
         if (curr != NULL) {
             while (curr != NULL) {
                 recycled[i] = *(void **)curr;
                 free((size_t *)curr - 1);
-                allocrecycled -= i;
+                atomic_fetch_sub_explicit(&allocrecycled, i, memory_order_relaxed);
                 curr = recycled[i];
             }
         }
@@ -94,25 +99,24 @@ void *mem_resize(void *old, size_t size) {
 
         if (size > old_size)
             memset((char *)new + old_size, 0, size - old_size);
-        allocrecycled -= size;
+        atomic_fetch_sub_explicit(&allocrecycled, size, memory_order_relaxed);
 
         if (old_size < MEMORY_MAXRECYCLED) {
             *(void **)old = recycled[old_size];
             recycled[old_size] = old;
-            allocrecycled += old_size;
+            atomic_fetch_add_explicit(&allocrecycled, old_size, memory_order_relaxed);
         } else {
             free(old_header);
         }
 
         if (size > old_size) {
-            alloctotal += size - old_size;
-            allocused += size - old_size;
+            atomic_fetch_add_explicit(&alloctotal, size - old_size, memory_order_relaxed);
+            atomic_fetch_add_explicit(&allocused, size - old_size, memory_order_relaxed);
         } else {
-            allocused -= old_size - size;
+            atomic_fetch_sub_explicit(&allocused, old_size - size, memory_order_relaxed);
         }
 
-        if (allocused > allocpeak)
-            allocpeak = allocused;
+        update_peak();
         return new;
     }
 
@@ -123,34 +127,33 @@ void *mem_resize(void *old, size_t size) {
     *new = size;
 
     if (size > old_size) {
-        alloctotal += size - old_size;
-        allocused += size - old_size;
+        atomic_fetch_add_explicit(&alloctotal, size - old_size, memory_order_relaxed);
+        atomic_fetch_add_explicit(&allocused, size - old_size, memory_order_relaxed);
     } else {
-        allocused -= old_size - size;
+        atomic_fetch_sub_explicit(&allocused, old_size - size, memory_order_relaxed);
     }
 
-    if (allocused > allocpeak)
-        allocpeak = allocused;
+    update_peak();
     return new + 1;
 }
 
 // info
 int mem_allocsize(void) {
-    return allocsize;
+    return atomic_load_explicit(&allocsize, memory_order_relaxed);
 }
 
 size_t mem_alloctotal(void) {
-    return alloctotal;
+    return atomic_load_explicit(&alloctotal, memory_order_relaxed);
 }
 
 size_t mem_allocused(void) {
-    return allocused;
+    return atomic_load_explicit(&allocused, memory_order_relaxed);
 }
 
 size_t mem_allocpeak(void) {
-    return allocpeak;
+    return atomic_load_explicit(&allocpeak, memory_order_relaxed);
 }
 
 size_t mem_allocrecycled(void) {
-    return allocrecycled;
+    return atomic_load_explicit(&allocrecycled, memory_order_relaxed);
 }
